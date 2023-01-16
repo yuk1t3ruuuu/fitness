4a96cf1cbbfe3961cde16636d95dc11d3752e2ea
Koki Someya <yuk1t3ru@icloud.com>
2023-01-17 01:46:33 +0900

    ドキュメント取得処理の追加

--- lib/todoOperation.dart
+++ lib/todoOperation.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:training/iconButton.dart';
@@ -5,7 +7,7 @@ import 'package:training/model.dart';
 
 
 Map<DateTime, List> eventsList = {}; //このリストにfirestoreからとってきたデータを格納する
-List eventContents = [];
+
 
 class todoOperation extends StatelessWidget {
    todoOperation({Key? key}) : super(key: key);
@@ -17,6 +19,13 @@ class todoOperation extends StatelessWidget {
     toFirestore: (todo, _) => todo.toJson(),
   );
 
+   final CollectionReference<ToDo> dateRef = FirebaseFirestore.instance
+       .collection('todos')
+       .withConverter<ToDo>(
+     fromFirestore: (snapshots, _) => ToDo.fromJson(snapshots.data()!),
+     toFirestore: (todo, _) => todo.toJson(),
+   );
+
 
   addToDo({String? description, DateTime? workDay}) async {
     final workSnapshot = await todoRef.get();
@@ -27,6 +36,23 @@ class todoOperation extends StatelessWidget {
           isCompleted: true,
         )
     );
+
+    final dateSnapshots = await dateRef.get();
+    final dateDocs = dateSnapshots.docs;
+    debugPrint('dateDocs中身$dateDocs');
+
+    for (final doc in dateDocs){
+      final CollectionReference<ToDo> workRef = doc.reference.collection('work')
+          .withConverter<ToDo>(
+        fromFirestore: (snapshots, _) => ToDo.fromJson(snapshots.data()!),
+        toFirestore: (todo, _) => todo.toJson()
+      );
+
+      final workSnapshot = await workRef.get();
+      final workDocs = workSnapshot.docs;
+      eventsList[DateTime.parse('$doc')] = workDocs;
+    }
+    debugPrint('eventsList中身$eventsList');
   }
 
 
